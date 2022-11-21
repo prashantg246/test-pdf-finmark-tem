0935b39f87a989d440d8e48c7437b30dc2f1aa8c
prashantg246 <109512753+prashantg246@users.noreply.github.com>
2022-11-21 18:37:28 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import{HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { SafePipe } from './safe.pipe';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
+  declarations: [ AppComponent, HelloComponent, SafePipe ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
