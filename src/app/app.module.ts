a651e9471f0af0421947fd27f0b4ee86abed8e49
prashantg246 <109512753+prashantg246@users.noreply.github.com>
2022-11-21 19:29:49 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import{HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SafePipe } from './safe.pipe';
 import { HttpRequestInterceptor } from './http-request-interceptor';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent, SafePipe ],
-  bootstrap:    [ AppComponent ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    NgxExtendedPdfViewerModule,
+  ],
+  declarations: [AppComponent, HelloComponent, SafePipe],
+  bootstrap: [AppComponent],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpRequestInterceptor,
       multi: true,
-    }
-  ]
+    },
+  ],
 })
-export class AppModule { }
+export class AppModule {}
